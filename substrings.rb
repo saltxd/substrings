1d0033c1ffa9170c1d3128c170b1ca84d26f9d89
marley <74526520+saltxd@users.noreply.github.com>
2023-07-12 06:34:54 -0600

    Fix spacing

--- substrings.rb
+++ substrings.rb
@@ -1,11 +1,15 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
 def substrings(word, dictionary)
   result = Hash.new(0)
   word = word.downcase
 
   dictionary.each do |substring|
     count = word.scan(substring).length
-    result[substring] += count if count >0
+    result[substring] += count if count > 0
   end
 
-  result
+  puts result
 end
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
